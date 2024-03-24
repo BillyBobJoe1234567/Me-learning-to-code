d3b996879791881d42c6d5752524b75feced481b
BillyBobJoe1234567 <160171587+BillyBobJoe1234567@users.noreply.github.com>
2024-03-24 18:40:05 -0400

    incorrect

--- Learntocodefile/Main.js
+++ Learntocodefile/Main.js
@@ -2,12 +2,16 @@ function alt() {
     var name1 = document.getElementById("Username").value;
     if (name1 === "Laurence") {
         alert("Welcome, " + name1 + "!");
+        window.location.href = "Main.html"
     } else if (name1 === "Henry") {
         alert("Go away "+name1+" please talk to Laurence to access this site.");
+        window.location.href = "incorrect.html"
     } else if (name1 === "Jefe"){
         alert("Welcome master, "+name1+". I am in your service.");
+        window.location.href = "Main.html"
     } else if (name1 === "Mum"){
         alert("Hello "+name1+", would you like some wine?")
+        window.location.href = "Main.html"
     } else {
         alert("Incorrect Username")
     }
